fc6c77bebeef91231b0a30b290ad314e3f53ea00
samueldlh <samueldlh.dev@gmail.com>
2021-11-08 22:52:14 -0500

    test nofitication

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.notifications.dev',
-  appName: 'ionic-notifications',
-  webDir: 'www',
-  bundledWebRuntime: false
-};
+	appId: 'com.notifications.dev',
+	appName: 'ionic-notifications',
+	webDir: 'www',
+	bundledWebRuntime: false,
+	plugins: {
+		PushNotifications: {
+			presentationOptions: ["badge", "sound", "alert"]
+		}
+	}
+}
 
 export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {
+	ActionPerformed,
+	PushNotificationSchema,
+	PushNotifications,
+	Token,
+	Channel,
+} from '@capacitor/push-notifications';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+	ngOnInit() {
+		console.log('Initializing HomePage');
 
-  constructor() {}
+		const channel: Channel = {
+			id: 'channel_test',
+			name: 'test',
+			importance: 1,
+			sound: 'boop.mp3'
+		}
 
+		// Request permission to use push notifications
+		// iOS will prompt user and return if they granted permission or not
+		// Android will just grant without prompting
+		PushNotifications.requestPermissions().then(result => {
+			if (result.receive === 'granted') {
+				// Register with Apple / Google to receive push via APNS/FCM
+				PushNotifications.register();
+			} else {
+				// Show some error
+			}
+		});
+
+		PushNotifications.addListener('registration', (token: Token) => {
+			alert('Push registration success, token: ' + token.value);
+		});
+
+		PushNotifications.addListener('registrationError', (error: any) => {
+			alert('Error on registration: ' + JSON.stringify(error));
+		});
+
+		PushNotifications.createChannel(channel).then(res => console.log(res));
+
+		PushNotifications.addListener(
+			'pushNotificationReceived',
+			(notification: PushNotificationSchema) => {
+				console.log('------------------- PushNotificationSchema -----------------');
+				alert('Push received: ' + JSON.stringify(notification));
+			},
+		);
+
+		PushNotifications.addListener(
+			'pushNotificationActionPerformed',
+			(notification: ActionPerformed) => {
+				console.log('------------------- ActionPerformed -----------------');
+				alert('Push action performed: ' + JSON.stringify(notification));
+			},
+		);
+	}
 }
